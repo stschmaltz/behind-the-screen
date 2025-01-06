99494b412a17caa2c425e2f04e714734eba72014
shane schmaltz <stschmaltz@gmail.com>
2025-01-05 21:16:32 -0700

    create first draft of encounter shape

--- pages/encounters.tsx
+++ pages/encounters.tsx
@@ -2,7 +2,91 @@ import Link from 'next/link';
 
 import { NextPage } from 'next';
 
+type Condition =
+  | 'blinded'
+  | 'charmed'
+  | 'deafened'
+  | 'frightened'
+  | 'grappled'
+  | 'incapacitated'
+  | 'invisible'
+  | 'paralyzed'
+  | 'petrified'
+  | 'poisoned'
+  | 'prone'
+  | 'restrained'
+  | 'stunned'
+  | 'unconscious';
+
+interface EncounterCharacter {
+  id: number;
+  name: string;
+  maxHP: number;
+  currentHP: number;
+  conditions: Condition[];
+  armorClass: number;
+}
+interface Encounter {
+  id: number;
+  name: string;
+  description?: string;
+  notes: string[];
+  enemies: EncounterCharacter[];
+  status: 'active' | 'inactive';
+  players: {
+    id: number;
+  }[];
+  npcs: EncounterCharacter[];
+  initiativeOrder: {
+    characterId: number;
+    initiative: number;
+  }[];
+  currentRound: number;
+  currentTurn: number;
+  createdAt: Date;
+}
+interface Player {
+  id: number;
+  name: string;
+  conditions: Condition[];
+  armorClass: number;
+}
+
 const EncountersPage: NextPage = () => {
+  const encounters: Encounter[] = [
+    {
+      id: 1,
+      name: 'Goblin Ambush',
+      description: 'A group of goblins ambush the party.',
+      notes: ['Goblins are hiding in the trees.'],
+      enemies: [
+        {
+          id: 1,
+          name: 'Goblin 1',
+          maxHP: 7,
+          currentHP: 7,
+          conditions: [],
+          armorClass: 15,
+        },
+        {
+          id: 2,
+          name: 'Goblin 2',
+          maxHP: 7,
+          currentHP: 7,
+          conditions: [],
+          armorClass: 15,
+        },
+      ],
+      status: 'inactive',
+      players: [{ id: 1 }],
+      npcs: [],
+      initiativeOrder: [],
+      currentRound: 0,
+      currentTurn: 0,
+      createdAt: new Date(),
+    },
+  ];
+
   return (
     <div className="flex h-full items-center justify-center flex-col">
       <div>Encounters</div>
@@ -12,9 +96,19 @@ const EncountersPage: NextPage = () => {
         </button>
       </Link>
       <ul className="list-disc">
-        <li>Encounter 1</li>
-        <li>Encounter 2</li>
-        <li>Encounter 3</li>
+        {encounters.map((encounter) => (
+          <li key={encounter.id}>
+            <Link href={`/encounters/${encounter.id}`}>
+              <div className="flex justify-between">
+                <div>{encounter.name}</div>
+                <div>{encounter.createdAt.toDateString()}</div>
+                <div>
+                  {encounter.status === 'active' ? 'Active' : 'Inactive'}
+                </div>
+              </div>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
