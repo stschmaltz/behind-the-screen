db15f45eaaa2ba499654b571f4c7dfa76705a7f7
Shane Schmaltz <stschmaltz@gmail.com>
2025-04-27 20:12:05 -0600

    add app updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,36 @@ import '../styles/global.css';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
+import { ReactElement, ReactNode } from 'react';
+import { NextPage } from 'next';
 import { CurrentUserProvider } from '../context/UserContext';
 import { usePWASetup } from '../hooks/use-pwa-setup.hook';
 import { Layout } from '../components/layout';
 
-export default function App({ Component, pageProps }: AppProps) {
+// Define types for pages with custom layouts
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
   usePWASetup();
+
+  // Use the layout defined at the page level, if available
+  const getLayout =
+    Component.getLayout ??
+    ((page) => (
+      <ThemeProvider>
+        <UserProvider user={pageProps.user}>
+          <CurrentUserProvider>
+            <Layout>{page}</Layout>
+          </CurrentUserProvider>
+        </UserProvider>
+      </ThemeProvider>
+    ));
 
   return (
     <>
@@ -20,16 +44,8 @@ export default function App({ Component, pageProps }: AppProps) {
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
       </Head>
-      <ThemeProvider>
-        <UserProvider user={pageProps.user}>
-          <CurrentUserProvider>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </CurrentUserProvider>
-        </UserProvider>
-        <Analytics />
-      </ThemeProvider>
+      {getLayout(<Component {...pageProps} />)}
+      <Analytics />
     </>
   );
 }
